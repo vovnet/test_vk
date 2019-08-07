2b60d04d92b327df1901afee513bd34d852d8180
vovnet <vovnet@yandex.ru>
2019-08-07 12:08:02 +0300

    some

---
+++ precache-manifest.7336a3c735cfb17806e69cccb944ab16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03060c2462a52e05ff11eb819e68b9ff",
+    "url": "/test_vk/index.html"
+  },
+  {
+    "revision": "c2a735d118aad85c18bf",
+    "url": "/test_vk/static/css/main.21177085.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/test_vk/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c2a735d118aad85c18bf",
+    "url": "/test_vk/static/js/main.78a36f1e.chunk.js"
+  },
+  {
+    "revision": "df9b0e6f1dd3f7853230",
+    "url": "/test_vk/static/js/runtime~main.86465cf9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_vk/static/media/logo.5d5d9eef.svg"
+  }
+]);
